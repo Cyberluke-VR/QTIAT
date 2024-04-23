6a6b212c99c9d6cae478113444844006fae4bb0c
Cyberluke-VR <70642617+Cyberluke-VR@users.noreply.github.com>
2024-04-23 12:48:00 +0100

    Create MHIAT.js

---
+++ MHIAT.js
@@ -0,0 +1,67 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension){
+	
+	var API = new APIConstructor();
+		  return stiatExtension({
+		  category : { 
+		    name : 'Mental Health Help Seeking', //Will appear in the data.
+		    title : {
+		      media : {word : 'Mental Health Help Seeking'}, //Name of the category presented in the task.
+		      css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+		      height : 7 //Used to position the "Or" in the combined block.
+		    }, 
+		    media : [ //Stimuli content as PIP's media objects
+    		    	{word : 'Therapy'}, 
+    			{word : 'Support'}, 
+    			{word : 'Psychologist'}, 
+    			{word : 'Treatment'}, 
+    			{word : 'Assistance'}, 
+    			{word : 'Support'}
+		    ], 
+		    //Stimulus css (style)
+		    css : {color:'#31b404','font-size':'3em'}
+		  },	
+
+  		attribute1 : 
+			{
+			name : 'Avoidance', //Attribute label
+			title : {
+				media : {word : 'Negative'}, //Name of the category presented in the task.
+				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+				height : 7 //Used to position the "Or" in the combined block.
+			}, 
+			media : [ //Stimuli
+				{word: 'Stigma'},
+				{word: 'Weakness'},
+				{word: 'Shame'},
+				{word: 'Failure'},
+				{word: 'Dependency'},
+				{word: 'Helplessness'}
+			], 
+			//Can change color and size of the targets here.
+			css : {color:'#31b404','font-size':'3em'}
+			},
+		attribute2 : 
+			{
+			name : 'Approach', //Attribute label
+			title : {
+				media : {word : 'Positive'}, //Name of the category presented in the task.
+				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+				height : 7 //Used to position the "Or" in the combined block.
+			}, 
+			media : [ //Stimuli
+				{word: 'Hope'},
+				{word: 'Wellness'},
+				{word: 'Growth'},
+				{word: 'Healing'},
+				{word: 'Strength'},
+				{word: 'Love'}
+			], 
+			//Can change color and size of the targets here.
+			css : {color:'#31b404','font-size':'3em'}
+			},
+
+  base_url : {//Where are your images at?
+    image : 'https://baranan.github.io/minno-tasks/images/'
+  }}
+  );
+  });
